7ac61befdb48857f319b3cc116c228584c513b5d
leolleo <leolleo.comp@gmail.com>
2016-05-29 12:24:36 -0300

    GPL info

--- scrpt2.R
+++ scrpt2.R
@@ -1,3 +1,23 @@
+#
+#    script R para realizar experimentos fatoriais 2^k sobre o valor 
+#    da evapotranspiração da cultura de referência
+#
+#    Copyright (C) 2015 Leonardo Cavalcante <leolleo.comp@gmail.com>    
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 is.installed <- function(mypkg) is.element(mypkg, installed.packages()[,1])
 
 #instala a biblioteca r2html se não estiver instalada
